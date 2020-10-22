37fd9b51a8aa5883543ab99e38b36011d4de9020
Sivakumar-U <usivakumarreddy@gmail.com>
2020-10-22 19:31:06 +0530

    Added: To validate last name

--- UserRegistration.java
+++ UserRegistration.java
@@ -5,10 +5,10 @@ import java.util.regex.Pattern;
 
 public class UserRegistration {
 	public static void main(String[] args) {
-		String fName = "siva";
+		String lName = "Upparapalli";
 		String pattern = "^[A-Z]{1}[a-z]{2,}$";
 		Pattern p = Pattern.compile(pattern);
-		Matcher m = p.matcher(fName);
+		Matcher m = p.matcher(lName);
 		if (m.matches())
 			System.out.println("Valid");
 		else
