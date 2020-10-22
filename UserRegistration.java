e6bc8d46af90360f70ca8fb9b59c0c417dca9871
Sivakumar-U <usivakumarreddy@gmail.com>
2020-10-22 19:57:08 +0530

    Added: To valide password with 8 characters

--- UserRegistration.java
+++ UserRegistration.java
@@ -5,10 +5,10 @@ import java.util.regex.Pattern;
 
 public class UserRegistration {
 	public static void main(String[] args) {
-		String mobileNo = "91-9178954621";
-		String pattern = "^[1-9]{2}-[1-9]{1}[0-9]{9}$";
+		String password = "siva142";
+		String pattern = "^[\\w]{8,}$";
 		Pattern p = Pattern.compile(pattern);
-		Matcher m = p.matcher(mobileNo);
+		Matcher m = p.matcher(password);
 		if (m.matches())
 			System.out.println("Valid");
 		else
